4322cdd6b228b33071c7276ab3ba24c955a26386
dos884 <dos884@gmail.com>
2019-03-22 12:16:30 +0200

    reverted main_old to the original file from deap: https://github.com/DEAP/deap/blob/82f774d9be6bad4b9d88272ba70ed6f1fca39fcf/examples/gp/symbreg.py

--- main_old.py
+++ main_old.py
@@ -1,4 +1,3 @@
-
 import operator
 import math
 import random
@@ -11,8 +10,6 @@ from deap import creator
 from deap import tools
 from deap import gp
 
-import matplotlib as mpl
-
 
 # Define new functions
 def protectedDiv(left, right):
@@ -20,17 +17,7 @@ def protectedDiv(left, right):
         return left / right
     except ZeroDivisionError:
         return 1
-    except OverflowError:
-        return 1
 
-
-def safePow(a,b):
-    try:
-        return (a**b).real
-    except ZeroDivisionError:
-        return 1
-    except OverflowError:
-        return 1
 
 pset = gp.PrimitiveSet("MAIN", 1)
 pset.addPrimitive(operator.add, 2)
@@ -38,12 +25,10 @@ pset.addPrimitive(operator.sub, 2)
 pset.addPrimitive(operator.mul, 2)
 pset.addPrimitive(protectedDiv, 2)
 pset.addPrimitive(operator.neg, 1)
-pset.addTerminal(2)
-pset.addPrimitive(safePow, 2)
 pset.addPrimitive(math.cos, 1)
 pset.addPrimitive(math.sin, 1)
-# pset.addEphemeralConstant("rand101", lambda: random.randint(-1, 1))
-# pset.renameArguments(ARG0='x')
+pset.addEphemeralConstant("rand101", lambda: random.randint(-1, 1))
+pset.renameArguments(ARG0='x')
 
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
@@ -55,47 +40,13 @@ toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("compile", gp.compile, pset=pset)
 
 
-def targetFunc(x):
-    #  the real function : x**4 + x**3 + x**2 + x
-    #return x ** 4 - x ** 3 - x ** 2 - x
-    #return x**2 +x**3 -x
-    return x**4 + x**3 + x**2 + x
-
-def empiricalLoss(func,points):
-    sqerrors = ((func(x[0]) - x[1]) ** 2 for x in points)
-    loss= (math.fsum(sqerrors) / len(points) )
-    return loss
-
-def empiricalFuncDiff(fun1,fun2,points):
-    sqerrors = [math.fabs(fun1(x) - fun2(x)) for x in points]
-    loss = (math.fsum(sqerrors) / len(points))
-    return loss
-
-OCCAM_PARAM = 1.0/10.0
-BIG_NUM=6*10**64
-
-import re
-def individualLength(individual):
-    st = individual.__str__().replace('(', ';')
-    A = re.split(";|,", st)
-    print(A)
-    return len(A)
-
 def evalSymbReg(individual, points):
     # Transform the tree expression in a callable function
     func = toolbox.compile(expr=individual)
     # Evaluate the mean squared error between the expression
-    loss = empiricalFuncDiff(func, targetFunc, points) #* (OCCAM_PARAM*len(individual.__str__()))
-    if loss<0.00000001:
-        loss = empiricalFuncDiff(func, targetFunc, points)
-        L=individualLength(individual)
-        a=0
-
-    elif loss>BIG_NUM:
-        loss = empiricalFuncDiff(func, targetFunc, points)
-        st = individual.__str__()
-
-    return loss,
+    # and the real function : x**4 + x**3 + x**2 + x
+    sqerrors = ((func(x) - x ** 4 - x ** 3 - x ** 2 - x) ** 2 for x in points)
+    return math.fsum(sqerrors) / len(points),
 
 
 toolbox.register("evaluate", evalSymbReg, points=[x / 10. for x in range(-10, 10)])
@@ -122,10 +73,9 @@ def main():
     mstats.register("min", numpy.min)
     mstats.register("max", numpy.max)
 
-    pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.1, 150, stats=mstats,
+    pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.1, 40, stats=mstats,
                                    halloffame=hof, verbose=True)
     # print log
-    print(hof.items[0].__str__())
     return pop, log, hof
 
 
